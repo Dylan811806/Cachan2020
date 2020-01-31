9f7a00f920bdbf153d1ab9fb37e9491748e2738e
Dylan811806 <60503211+Dylan811806@users.noreply.github.com>
2020-01-31 11:28:19 +0100

    Create Global.h

---
+++ Global.h
@@ -0,0 +1,8 @@
+#include "mbed.h"
+#define MIN_CENTRE// calibrer la cam pour definir ce qu'on considere comme centree
+#define MAX_CENTRE
+#define LOIN//calibrer pour savoir ce qu'on considere comme proche ou loin
+#define PROCHE
+void Follow_balle(float,float,float);
+int Force_virage;
+int Vit_moy;
