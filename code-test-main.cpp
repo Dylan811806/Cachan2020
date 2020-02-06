8d73bb126b619579e8521a41ddc88a19a32b0de0
Dylan811806 <60503211+Dylan811806@users.noreply.github.com>
2020-02-06 17:08:57 +0100

    Create code-test-main.cpp

---
+++ code-test-main.cpp
@@ -0,0 +1,18 @@
+
+#include "mbed.h"
+//#include "Global.h"
+#include "deplacement.h"
+
+
+
+
+////////////////////////////MACHINE A ETAT V.0//////////////////////////////////
+int main()
+{
+    
+    while(1){
+    bluetooth();
+    //TEST_1();
+    }
+}
+    
