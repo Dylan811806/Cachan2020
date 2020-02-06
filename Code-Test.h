31ac2657853656141f5cdbcef69574676a7d0193
Dylan811806 <60503211+Dylan811806@users.noreply.github.com>
2020-02-06 17:07:14 +0100

    Create Code-Test.h

---
+++ Code-Test.h
@@ -0,0 +1,29 @@
+//PROTOTYPE FONCTIONS DEPLACEMENT
+
+void ahead(int);        //entree: int[-100(full AR); +100(full AV)] - vmoy
+                       
+
+void turn(int, int);    //entrees: int[-100(full G); +100(full D)] - taux de virage
+                        //  a terme -->     int[-100(full AR); +100(full AV)] - vmoy virage
+                        //   
+                        //
+                        //
+                        //
+                        //
+                        //
+
+                                
+void stop (void);// arret moteur  
+
+void rotatoi_complete (bool);              
+
+                        
+void TEST_1 (void);
+void bluetooth(void);
+
+           
+           
+           
+           
+           
+           
