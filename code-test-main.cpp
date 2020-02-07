8683d6272078692708b458479865028c39da5f9b
Dylan811806 <60503211+Dylan811806@users.noreply.github.com>
2020-02-07 12:25:53 +0100

    Update code-test-main.cpp

--- code-test-main.cpp
+++ code-test-main.cpp
@@ -1,18 +1,14 @@
-
 #include "mbed.h"
 //#include "Global.h"
 #include "deplacement.h"
 
-
-
-
 ////////////////////////////MACHINE A ETAT V.0//////////////////////////////////
 int main()
 {
-    
+    Init();
     while(1){
     bluetooth();
     //TEST_1();
+   //test();
     }
 }
-    
