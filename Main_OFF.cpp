5792c1403af97f8f5f5be694d215890a14031185
Dylan811806 <60503211+Dylan811806@users.noreply.github.com>
2020-03-19 12:06:03 +0100

    Create Main_OFF.cpp

---
+++ Main_OFF.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+#include "Global.h"
+#include "MoteursDeplacement.h"
+#include "MoteurBalais.h"
+#include "FonctionRasp.h"
+#include "FonctionCapteur.h"
+
+int main()
+{
+    Init();//Init des moteurs
+    while(1){
+    bluetooth();// programme de test des fonctions moteur via bluetooth
+    //TEST_1(); // programme avec mouvement prédefinit
+   //test();
+    }
+}
