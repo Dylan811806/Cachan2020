5dc9af282d318302b25e3cd9415c2ed5604c529c
Dylan811806 <60503211+Dylan811806@users.noreply.github.com>
2020-03-26 14:18:52 +0100

    Update MoteursDeplacement.cpp

--- FonctionMoteurs/MoteurDeplacement/MoteursDeplacement.cpp
+++ FonctionMoteurs/MoteurDeplacement/MoteursDeplacement.cpp
@@ -1,5 +1,26 @@
 #include "MoteursDeplacement.h"
 #include "mbed.h"
+
+void Follow_balle(float,float,float);
+int Force_virage;
+int Vit_moy;
+Serial Bt(PC_10,PC_11);
+PwmOut MotL(PC_6);//2eme connecteur
+PwmOut MotR(PC_8);//1er connecteur , 3eme sur pc9, le 4eme sur pc8,5eme pc1 pc0 analogin,6eme pc2 et pc3
+DigitalOut SensL(PC_14,0);
+DigitalOut SensR(PC_15,0);//sens mot 3 ,4eme ph1
+//DigitalOut SensTest(PC_13,0);
+//DigitalOut SensL1(PC_14,1);
+//DigitalOut SensR1(PC_15,1);
+///DigitalOut TestPinPh(PH_0,1);// MARCHE PAS a oublier
+DigitalOut TestPinPH1(PH_1,1);
+int etat=0;
+int iVmoy;
+int iTurnRate;
+int tempo_ms=1000;//peut changer
+Timer T1;     
+char commande_bt='g';
+
 /*void follow_balle(int x,int z)// a definir ce qu'on recup de la cam
     {
         switch(x){//covertion de ce qui l'envoie obligatoire entre -100 et 100
