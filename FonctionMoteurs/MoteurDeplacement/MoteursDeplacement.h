548b99a9fd563c8d3a1a650b028f9d920a2ce28f
Dylan811806 <60503211+Dylan811806@users.noreply.github.com>
2020-03-26 14:18:16 +0100

    Update MoteursDeplacement.h

--- FonctionMoteurs/MoteurDeplacement/MoteursDeplacement.h
+++ FonctionMoteurs/MoteurDeplacement/MoteursDeplacement.h
@@ -11,25 +11,7 @@
 #define MAX_CENTRE
 #define LOIN//calibrer pour savoir ce qu'on considere comme proche ou loin
 #define PROCHE
-void Follow_balle(float,float,float);
-int Force_virage;
-int Vit_moy;
-Serial Bt(PC_10,PC_11);
-PwmOut MotL(PC_6);//2eme connecteur
-PwmOut MotR(PC_8);//1er connecteur , 3eme sur pc9, le 4eme sur pc8,5eme pc1 pc0 analogin,6eme pc2 et pc3
-DigitalOut SensL(PC_14,0);
-DigitalOut SensR(PC_15,0);//sens mot 3 ,4eme ph1
-//DigitalOut SensTest(PC_13,0);
-//DigitalOut SensL1(PC_14,1);
-//DigitalOut SensR1(PC_15,1);
-///DigitalOut TestPinPh(PH_0,1);// MARCHE PAS a oublier
-DigitalOut TestPinPH1(PH_1,1);
-int etat=0;
-int iVmoy;
-int iTurnRate;
-int tempo_ms=1000;//peut changer
-Timer T1;     
-char commande_bt='g';
+
 
 void ahead(int);        //entree: int[-100(full AR); +100(full AV)] - vmoy
                        
